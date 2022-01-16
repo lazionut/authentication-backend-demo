df75fc8045e023137e6900ec1ddcf1178abf5da0
lazionut <lionut@mailbox.org>
2022-01-16 12:27:19 +0200

    Some HTTP response codes changed

--- typeorm-express-sample/src/middleware/authToken.ts
+++ typeorm-express-sample/src/middleware/authToken.ts
@@ -8,14 +8,14 @@ const authToken = (request: Request, response: Response, next: NextFunction) => 
     const token = request.body.token;
 
     if (!token) {
-        response.status(404);
+        response.status(500);
         return next("Token required");
     }
     try {
         const decoded = jwt.verify(token, jwtSecret);
         response.user = decoded;
     } catch (err) {
-        response.status(404);
+        response.status(500);
         return next("Invalid token");
     }
 }
